f1b177ac02de73963b2fa033ef292e81e4c2fa9e
Mats Larsson <matslarsson@Matss-MacBook-Air.local>
2017-12-04 22:02:52 +0100

    Variables.java

---
+++ Variable.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Variable extends Atom {
+	private String name;
+
+	public Variable(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Sexpr eval(HashMap<String, Sexpr> variables) {
+		if (variables.containsKey(this.name)) {
+			return variables.get(this.name);
+		} else {
+			return this;
+		}
+	}
+}
+
